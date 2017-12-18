aa895b150a41913b7fc3ddc14102c238729bbe23
Jonathan Plata <jonathan_plata@ymail.com>
2017-12-18 14:07:37 -0800

    images

--- click-game/src/App.js
+++ click-game/src/App.js
@@ -2,23 +2,19 @@ import React, { Component } from 'react';
 import './App.css';
 import ImageCard from './components/ImageCard';
 import Images from './images.json';
-console.log(Images[0])
+
 class App extends Component {
   render() {
-    return (
+    return ( 
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome</h1>
         </header>
         <p></p>
         <div className="Content">
-          
-          {Images.map(obj => (
-            <ImageCard 
-              name={obj.name} 
-              img={obj.image}
-            />
-          ))}
+          <ImageCard
+            array={Images}
+          />     
         </div>
       </div>
     );
--- click-game/src/components/ImageCard/ImageCard.js
+++ click-game/src/components/ImageCard/ImageCard.js
@@ -1,14 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
 import "./ImageCard.css";
+import Images from '../../images.json';
 
-const ImageCard = props => (
+class ImageCard extends Component {
+  constructor(props) {
+  	super(props);
+    this.state = {
+      match: null
+    };
+  	this.handleClick = this.handleClick.bind(this);
+  }
 
-  <div className="card">
-  	<div className="img-container">
-  		<img alt={props.name} src={props.img}/>
-  	</div>
-  	<p>{props.name}</p>
-  </div>
-);
+  componentDidMount() {
+    this.setState({match: 0}, () => {
+      console.log(this.state.match);
+    });
+    console.log(Images);
+  }
+
+  handleClick(id, e) {
+    const num = id;
+
+    console.log(num);
+    console.log(this.state);
+    console.log(this.state.match);
+
+    if(this.state.match === 0 && this.state.match !== typeof Number ) {
+      this.setState({match: id}, () => {
+        console.log(this.state.match);
+      });
+    } else {
+      // if (this.state !== null && this.state.match === num) {
+      //   console.log("got a match");
+      // }
+    }
+  }
+
+  render(props) {
+  	return(
+      {Images.map(function(obj) {
+        <img 
+
+        />
+      })}
+  	  <img
+  	  	key={this.props.array[0].id}
+        id={this.props.array[0].id}
+   	  	alt={this.props.array[0].name}
+  	  	src={this.props.array[0].image}
+  	  	onClick={this.handleClick.bind(this, this.props.id)}
+  	  />
+  	);
+  }
+}
 
 export default ImageCard;
